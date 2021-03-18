ef22ef0636d0548d7b5f6bfd04288f889a196db8
David Jones <coreldavid@hotmail.com>
2021-03-18 01:18:41 -0400

    First commit

---
+++ src/main/java/com/springboot/app/api/SimpleSprinbootAppApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.app.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleSprinbootAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleSprinbootAppApplication.class, args);
+	}
+
+}
