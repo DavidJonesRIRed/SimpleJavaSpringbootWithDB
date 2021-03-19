e0386aeb5376a2114f4e0b1ba492afa9a1155d2b
David Jones <coreldavid@hotmail.com>
2021-03-18 22:27:23 -0400

    started student api

--- src/main/java/com/springboot/app/api/SimpleSprinbootAppApplication.java
+++ src/main/java/com/springboot/app/api/SimpleSprinbootAppApplication.java
@@ -1,33 +1,13 @@
 package com.springboot.app.api;
 
-import com.springboot.app.api.student.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.time.LocalDate;
-import java.time.Month;
-import java.util.List;
 
 @SpringBootApplication
-@RestController
 public class SimpleSprinbootAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SimpleSprinbootAppApplication.class, args);
 	}
 
-	@GetMapping
-	public List<Student> hello(){
-		return List.of(
-				new Student(
-						1l,
-						"Marian",
-						"mariam.jamal@gmail.com",
-						LocalDate.of(2000, Month.JANUARY,5),
-						21
-				)
-		);
-	}
 }
